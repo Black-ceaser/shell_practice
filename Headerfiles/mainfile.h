74aaf9819125526fb651a93bbc5b74712885a230
Black-ceaser <calvin.annan@gmail.com>
2023-10-23 16:30:10 +0000

    sorted the various files into various directories

---
+++ Headerfiles/mainfile.h
@@ -0,0 +1,18 @@
+#ifndef MY_HEADER
+#define MY_HEADER
+
+#include <stdio.h>
+int _putchar(char c);
+void alphabet(void);
+void alphabet_x10(void);
+int _lowercase(int c);
+int _uppercase(int c);
+int sign(int n);
+int abs(int);
+int last_digit(int);
+void jackbauer(void);
+void timesTable(void);
+int add(int, int);
+void printTo98(int n);
+
+#endif /*MY_HEADER*/
